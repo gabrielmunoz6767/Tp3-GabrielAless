8f785d0a097d7a27301e883dc2fd110599be850e
gabrielmunoz6767 <mgabrieljosuemarinmunos@gmail.com>
2026-06-30 13:50:03 -0600

    Actualizar formato de reporte de cierre diario para incluir símbolo de colón y corregir nombre de archivo

--- factura.py
+++ factura.py
@@ -105,14 +105,14 @@ def generarReporteCierreDiario(listaRegistros, totalesPorTipoPago, montoTotalDia
     """
     ahora = datetime.now()
     fechaStr = ahora.strftime("%d-%m-%Y")
-    nombreArchivo = "reporte_cierre_diario_" + fechaStr + ".pdf"
+    nombreArchivo = "reporteDecierreDiario_" + fechaStr + ".pdf"
 
     pdf = FPDF()
     pdf.add_page()
 
     pdf.set_text_color(0, 51, 102)
     pdf.set_font("Arial", style="B", size=18)
-    pdf.cell(0, 12, txt="REPORTE DE CIERRE DIARIO", ln=1, align="C")
+    pdf.cell(0, 12, txt="REPORTE DE CIERRE DIARIO", ln=1, align="₡")
 
     pdf.set_text_color(80, 80, 80)
     pdf.set_font("Arial", style="I", size=11)
@@ -125,26 +125,26 @@ def generarReporteCierreDiario(listaRegistros, totalesPorTipoPago, montoTotalDia
     pdf.set_fill_color(0, 51, 102)
     pdf.set_font("Arial", style="B", size=9)
     for i in range(len(encabezados)):
-        pdf.cell(anchos[i], 8, txt=encabezados[i], border=1, fill=True, align="C")
+        pdf.cell(anchos[i], 8, txt=encabezados[i], border=1, fill=True, align="₡")
     pdf.ln()
 
     pdf.set_text_color(0, 0, 0)
     pdf.set_font("Arial", size=9)
     for fila in listaRegistros:
         for i in range(len(fila)):
-            pdf.cell(anchos[i], 7, txt=str(fila[i]), border=1, align="C")
+            pdf.cell(anchos[i], 7, txt=str(fila[i]), border=1, align="₡")
         pdf.ln()
 
     pdf.ln(8)
     pdf.set_text_color(0, 102, 51)
     pdf.set_font("Arial", style="B", size=11)
     for tipoPago in totalesPorTipoPago:
-        pdf.cell(0, 7, txt="Total " + tipoPago + ": C" + str(totalesPorTipoPago[tipoPago]), ln=1)
+        pdf.cell(0, 7, txt="Total " + tipoPago + ": ₡" + str(totalesPorTipoPago[tipoPago]), ln=1)
 
     pdf.ln(4)
     pdf.set_text_color(153, 0, 0)
     pdf.set_font("Arial", style="B", size=14)
-    pdf.cell(0, 10, txt="TOTAL RECAUDADO DEL DIA: C" + str(montoTotalDia), ln=1)
+    pdf.cell(0, 10, txt="TOTAL RECAUDADO DEL DIA: ₡" + str(montoTotalDia), ln=1)
 
     pdf.output(nombreArchivo)
     return nombreArchivo
